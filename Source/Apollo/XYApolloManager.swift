5b1bf215175ff7d9f1445954f77daa1b791f5a78
Darren Sutherland <darren@xyo.network>
2019-02-04 11:02:01 -0800

    starting to flesh out manager

--- Source/Apollo/XYApolloManager.swift
+++ Source/Apollo/XYApolloManager.swift
@@ -7,16 +7,24 @@
 //
 
 import Apollo
+import Foundation
 
 public class XYApolloManager {
 
     fileprivate static let endpointUrl = "https://cmsltk3yhg.execute-api.us-east-1.amazonaws.com/dev/graphql"
 
+    fileprivate let queue: DispatchQueue
+
     public let client: ApolloClient
 
-    public init?() {
+    public init?(queue: DispatchQueue = DispatchQueue.main) {
         guard let validUrl = URL(string: XYApolloManager.endpointUrl) else { return nil }
         self.client = ApolloClient(url: validUrl)
+        self.queue = queue
+    }
+
+    public func fetch<Query: GraphQLQuery>(for query: Query, then callback: OperationResultHandler<Query>?) {
+        self.client.fetch(query: query, resultHandler: callback)
     }
 
 }
