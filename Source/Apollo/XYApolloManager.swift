5708eae3416fd9b12ecaf1f26587fd2ca8da68ec
Darren Sutherland <darren@xyo.network>
2019-02-05 14:47:18 -0800

    mutate

--- Source/Apollo/XYApolloManager.swift
+++ Source/Apollo/XYApolloManager.swift
@@ -10,14 +10,19 @@ import Apollo
 import Foundation
 
 public protocol XYQueryManager: class {
+    var cache: ApolloStore { get }
+
     func fetch<Query: GraphQLQuery>(for query: Query, then callback: OperationResultHandler<Query>?)
     func watch<Query: GraphQLQuery>(for query: Query, then callback: @escaping OperationResultHandler<Query>) -> GraphQLQueryWatcher<Query>
+    func mutate<Mutation: GraphQLMutation>(for mutation: Mutation, then callback: @escaping OperationResultHandler<Mutation>)
 }
 
 public class XYApolloQueryManager {
 
     private let client: ApolloClient
     private let queue: DispatchQueue
+
+    fileprivate static let store = ApolloStore(cache: InMemoryNormalizedCache())
 
     private static let xyAuthHeader = "X-Auth-Token"
     private static let endpointUrl = "https://cmsltk3yhg.execute-api.us-east-1.amazonaws.com/dev/graphql"
@@ -38,14 +43,17 @@ public class XYApolloQueryManager {
 public extension XYApolloQueryManager {
 
     public class func nonAuth(on queue: DispatchQueue = .main) -> XYApolloQueryManager {
-        return XYApolloQueryManager(client: ApolloClient(url: serverUrl), queue: queue)
+        let client = ApolloClient(networkTransport: HTTPNetworkTransport(url: serverUrl), store: store)
+        client.cacheKeyForObject = { $0["id"] }
+        return XYApolloQueryManager(client: client, queue: queue)
     }
 
     public class func auth(token: String, on queue: DispatchQueue = .main) -> XYApolloQueryManager {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = [xyAuthHeader: token]
 
-        let client = ApolloClient(networkTransport: HTTPNetworkTransport(url: serverUrl, configuration: configuration))
+        let client = ApolloClient(networkTransport: HTTPNetworkTransport(url: serverUrl, configuration: configuration), store: store)
+        client.cacheKeyForObject = { $0["id"] }
         return XYApolloQueryManager(client: client, queue: queue)
     }
 
@@ -53,12 +61,20 @@ public extension XYApolloQueryManager {
 
 extension XYApolloQueryManager: XYQueryManager {
 
+    public var cache: ApolloStore {
+        return XYApolloQueryManager.store
+    }
+
     public func fetch<Query: GraphQLQuery>(for query: Query, then callback: OperationResultHandler<Query>?) {
         self.client.fetch(query: query, queue: self.queue, resultHandler: callback)
     }
 
     public func watch<Query: GraphQLQuery>(for query: Query, then callback: @escaping OperationResultHandler<Query>) -> GraphQLQueryWatcher<Query> {
-        return self.client.watch(query: query, resultHandler: callback)
+        return self.client.watch(query: query, queue: self.queue, resultHandler: callback)
+    }
+
+    public func mutate<Mutation: GraphQLMutation>(for mutation: Mutation, then callback: @escaping OperationResultHandler<Mutation>) {
+        self.client.perform(mutation: mutation, queue: self.queue, resultHandler: callback)
     }
 
 }
