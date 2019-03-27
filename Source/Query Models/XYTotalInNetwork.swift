665ee1e378c53e19361a634faf307ffc7aef5c60
Darren Sutherland <darren@xyo.network>
2019-03-27 14:40:47 -0700

    completed total query

--- Source/Query Models/XYTotalInNetwork.swift
+++ Source/Query Models/XYTotalInNetwork.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2019 XYO Network. All rights reserved.
 //
 
-import Foundation
+import Apollo
+
+final public class XYTotalInNetworkQuery: XYQuery {
+    public typealias QueryModel = GetTotalInNetworkQuery.Data.Block.Pagination
+
+    public var queryData = XYQueryData<GetTotalInNetworkQuery, QueryModel>()
+
+    public let queryManager: XYQueryManager
+    public var watcher: GraphQLQueryWatcher<GetTotalInNetworkQuery>?
+    public var listeners: [String : (GraphQLResult<GetTotalInNetworkQuery.Data>?, Error?) -> ()] = [:]
+
+    public init(with authToken: String) {
+        self.queryManager = XYApolloQueryManager.auth(token: authToken)
+        self.watcher = self.queryManager.watch(for: GetTotalInNetworkQuery(), then: self.processResponse)
+        self.queryData.setConvertor { queryData in
+            return queryData.data?.blocks?.pagination?.count != nil ?
+            [QueryModel(count: (queryData.data?.blocks?.pagination?.count)!)] : []
+        }
+    }
+}
